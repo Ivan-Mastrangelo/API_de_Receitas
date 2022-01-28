f6b99c6a44193b32b468770ea26b8fb9356f60d7
jmporto <jmporto@outlook.com>
2022-01-28 18:36:26 -0300

    feat: pagina de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
-function Profile() {
-  return <div> </div>;
+export default function Perfil() {
+  const response = localStorage.getItem('user');
+  const user = JSON.parse(response);
+  const HISTORY = useHistory();
+
+  const handleClick = () => {
+    localStorage.clear();
+    const path = '/';
+    HISTORY.push(path);
+  };
+
+  return (
+    <>
+      <h2 data-testid="profile-email">{user.email}</h2>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">
+          Done Recipes
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button type="button" data-testid="profile-favorite-btn">
+          Favorite Recipes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ handleClick }
+      >
+        Logout
+      </button>
+    </>
+  );
 }
-
-export default Profile;
