be9de1ca2355e0a27ebfcdb5b588e7fa099803ac
Luiz Ricardo <luizricardo41@gmail.com>
2022-01-31 16:15:18 -0300

    feat: Refatoraçao da pagina de perfil, para importaçao do email via localStorage

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-export default function Perfil() {
-  const response = localStorage.getItem('user');
-  const user = JSON.parse(response);
+export default function Profile() {
+  const [login, setLogin] = useState([]);
+  useEffect(() => {
+    const response = localStorage.getItem('user');
+    const user = JSON.parse(response);
+    setLogin(user);
+  }, []);
+
   const HISTORY = useHistory();
 
   const handleClick = () => {
@@ -28,7 +33,7 @@ export default function Perfil() {
         nameIcon2="explore-icon"
         iconId2="search-top-btn"
       />
-      <h2 data-testid="profile-email">{user.email}</h2>
+      <h2 data-testid="profile-email">{login && login.email}</h2>
       <Link to="/done-recipes">
         <button type="button" data-testid="profile-done-btn">
           Done Recipes
