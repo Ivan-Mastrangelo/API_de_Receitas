11afad47b085326b56f4dac7c51926ab285545bf
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-31 18:27:52 -0300

    Adiciona requisição a API de todas as comidas

--- src/services/foodAPI.js
+++ src/services/foodAPI.js
@@ -42,6 +42,17 @@ export async function getRandomMeal() {
   }
 }
 
+// Retorna todas as receitas de comidas
+export async function getAllMeal() {
+  try {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 // retorna foto do prato
 // export async function getPics(ingrediente) {
 //   try {
