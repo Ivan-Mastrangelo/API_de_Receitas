f9efbf4d54705578ace185796dc73e8fdd289b6b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-01-31 18:04:13 -0300

    adiciona a importação do useContext para trazer os estados das comidas e bebidas dos respectivos Providers

--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import DrinkContext from '../context/DrinkContext';
+import FoodContext from '../context/FoodContext';
 
 function Foods() {
+  const { food, setFood } = useContext(FoodContext);
+  const { drink, setDrink } = useContext(DrinkContext);
+
   return (
-    <Header
-      icon1={ profileIcon }
-      nameIcon1="profile-icon"
-      iconId1="profile-top-btn"
-      pageName="Foods"
-      nameId="page-title"
-      icon2={ searchIcon }
-      nameIcon2="explore-icon"
-      iconId2="search-top-btn"
-    />
+    <>
+      <Header
+        icon1={ profileIcon }
+        nameIcon1="profile-icon"
+        iconId1="profile-top-btn"
+        pageName="Foods"
+        nameId="page-title"
+        icon2={ searchIcon }
+        nameIcon2="explore-icon"
+        iconId2="search-top-btn"
+      />
+      <div>
+        comidas
+      </div>
+      <div>
+        bebidas
+      </div>
+    </>
   );
 }
 
