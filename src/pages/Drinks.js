8652f03136f3b77e7c91b2c843728c2148864c4e
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-02-01 11:04:48 -0300

    Insere componente à página

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
+import DrinkCards from '../components/DrinkCards';
 
 function Drinks() {
   return (
-    <Header
-      icon1={ profileIcon }
-      nameIcon1="profile-icon"
-      iconId1="profile-top-btn"
-      pageName="Drinks"
-      nameId="page-title"
-      icon2={ searchIcon }
-      nameIcon2="explore-icon"
-      iconId2="search-top-btn"
-    />
+    <>
+      <Header
+        icon1={ profileIcon }
+        nameIcon1="profile-icon"
+        iconId1="profile-top-btn"
+        pageName="Drinks"
+        nameId="page-title"
+        icon2={ searchIcon }
+        nameIcon2="explore-icon"
+        iconId2="search-top-btn"
+      />
+      <DrinkCards />
+    </>
   );
 }
 
