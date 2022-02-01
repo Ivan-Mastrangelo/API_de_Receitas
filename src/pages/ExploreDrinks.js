67d13632c5f5522fbea0b95f1f4a643ba8a1d2e9
jmporto <jmporto@outlook.com>
2022-02-01 15:56:10 -0300

    feat: tela de explorar bebidas e comidas, req 70-74

--- src/pages/ExploreDrinks.js
+++ src/pages/ExploreDrinks.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+// import Footer from '../components/Footer';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
+
+  const fetchAPI = async () => {
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+    const { drinks } = await fetch(url).then((response) => response.json());
+    return drinks;
+  };
+
+  const handleClick = async () => {
+    const fetchResult = await fetchAPI();
+    history.push(`/drinks/${fetchResult[0].idDrink}`);
+  };
+
   return (
-    <Header
-      icon1={ profileIcon }
-      nameIcon1="profile-icon"
-      iconId1="profile-top-btn"
-      pageName="Explore Drinks"
-      nameId="page-title"
-      icon2={ searchIcon }
-      nameIcon2="explore-icon"
-      iconId2="search-top-btn"
-    />
+    <>
+      <Header
+        icon1={ profileIcon }
+        nameIcon1="profile-icon"
+        iconId1="profile-top-btn"
+        pageName="Explore Drinks"
+        nameId="page-title"
+        icon2={ searchIcon }
+        nameIcon2="explore-icon"
+        iconId2="search-top-btn"
+      />
+      <Link to="/explore/drinks/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ handleClick }
+      >
+        Surprise me!
+      </button>
+      {/* <Footer /> */}
+    </>
   );
 }
--- src/pages/ExploreFood.js
+++ src/pages/ExploreFood.js
@@ -1,21 +1,60 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+// import Footer from '../components/Footer';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-function ExploreFood() {
+export default function ExploreFood() {
+  const history = useHistory();
+
+  const fetchAPI = async () => {
+    const url = 'https://www.themealdb.com/api/json/v1/1/random.php';
+    const { meals } = await fetch(url).then((response) => response.json());
+    return meals;
+  };
+
+  const handleClick = async () => {
+    const fetchResult = await fetchAPI();
+    history.push(`/foods/${fetchResult[0].idMeal}`);
+  };
+
   return (
-    <Header
-      icon1={ profileIcon }
-      nameIcon1="profile-icon"
-      iconId1="profile-top-btn"
-      pageName="Explore Foods"
-      nameId="page-title"
-      icon2={ searchIcon }
-      nameIcon2="explore-icon"
-      iconId2="search-top-btn"
-    />
+    <>
+      <Header
+        icon1={ profileIcon }
+        nameIcon1="profile-icon"
+        iconId1="profile-top-btn"
+        pageName="Explore Foods"
+        nameId="page-title"
+        icon2={ searchIcon }
+        nameIcon2="explore-icon"
+        iconId2="search-top-btn"
+      />
+      <Link to="/explore/foods/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
+      <Link to="/explore/foods/nationalities">
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+        >
+          By Nationality
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ handleClick }
+      >
+        Surprise me!
+      </button>
+      {/* <Footer /> */}
+    </>
   );
 }
-
-export default ExploreFood;
