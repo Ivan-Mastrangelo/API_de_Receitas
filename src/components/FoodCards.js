f4460f2e5c50e57c83d57e9cad972fa011047497
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-02-01 10:55:10 -0300

    cria componente FoodCard

--- src/components/FoodCards.js
+++ src/components/FoodCards.js
@@ -2,6 +2,25 @@ import React, { useContext } from 'react';
 import FoodContext from '../context/FoodContext';
 
 export default function FoodCards() {
-  const { food, setFood } = useContext(FoodContext);
-  return <div>comidas</div>;
+  const { food } = useContext(FoodContext);
+  const doze = 12;
+
+  return (
+    <div>
+      {
+        food.meals && food.meals
+          .filter((_, index) => index < doze)
+          .map(({ id, strMealThumb, strMeal }, index) => (
+            <div key={ id } data-testid={ `${index}-recipe-card` }>
+              <span data-testid={ `${index}-card-name` }>{strMeal}</span>
+              <img
+                src={ `${strMealThumb}/preview` }
+                alt={ strMeal }
+                data-testid={ `${index}-card-img` }
+              />
+            </div>
+          ))
+      }
+    </div>
+  );
 }
