7837998a03fca4e050df30d17be00ab2cdc1e3c5
Luiz Ricardo <luizricardo41@gmail.com>
2022-02-02 15:55:58 -0300

    Refatoraçao para renderizaçao dos cards

--- src/components/FoodCards.js
+++ src/components/FoodCards.js
@@ -2,27 +2,13 @@ import React, { useContext } from 'react';
 import FoodContext from '../context/FoodContext';
 
 export default function FoodCards() {
-  const { food, mealByCategory: { actionBtn, apiKey } } = useContext(FoodContext);
+  const { auxFood: { recipe: { meals } } } = useContext(FoodContext);
   const doze = 12;
 
   return (
     <div>
       {
-        actionBtn && apiKey.filter((_, index) => index < doze)
-          .map(({ strMeal, strMealThumb }, index) => (
-            <div key={ index } data-testid={ `${index}-recipe-card` }>
-              <span data-testid={ `${index}-card-name` }>{strMeal}</span>
-              <img
-                data-testid={ `${index}-card-img` }
-                src={ strMealThumb }
-                alt={ strMeal }
-                style={ { width: '100px' } }
-              />
-            </div>
-          ))
-      }
-      {
-        food.meals && !actionBtn && food.meals
+        meals && meals
           .filter((_, index) => index < doze)
           .map(({ strMealThumb, strMeal }, index) => (
             <div key={ index } data-testid={ `${index}-recipe-card` }>
